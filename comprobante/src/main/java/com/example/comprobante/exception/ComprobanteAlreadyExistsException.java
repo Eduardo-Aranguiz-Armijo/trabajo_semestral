b8f46d269b9b580cbaf35e3cf2199befe3b64a8a
Edward Cardoza Mesina <ed.cardoza@duocuc.cl>
2026-05-22 14:44:46 -0400

    Delete comprobante directory

--- comprobante/src/main/java/com/example/comprobante/exception/ComprobanteAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.example.comprobante.exception;
-
-public class ComprobanteAlreadyExistsException extends RuntimeException {
-
-    public ComprobanteAlreadyExistsException(Long paymentId) {
-        super("Ya existe un comprobante para el pago: " + paymentId);
-    }
-}
