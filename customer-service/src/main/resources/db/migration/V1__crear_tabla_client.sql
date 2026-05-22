bc937434764f15f7b68012824c14a0fabf6521f1
Edward Cardoza Mesina <ed.cardoza@duocuc.cl>
2026-05-22 14:44:54 -0400

    Delete customer-service directory

--- customer-service/src/main/resources/db/migration/V1__crear_tabla_client.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE clients (
-                        id_customer BIGINT AUTO_INCREMENT PRIMARY KEY,
-                        name VARCHAR(255) NOT NULL,
-                        rut VARCHAR(50) NOT NULL UNIQUE,
-                        email VARCHAR(255) NOT NULL UNIQUE,
-                        phone VARCHAR(50) NOT NULL,
-                        address VARCHAR(255) NOT NULL,
-                        user_id BIGINT NOT NULL UNIQUE
-);
