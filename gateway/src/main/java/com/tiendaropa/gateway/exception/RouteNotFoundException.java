50a4215aa66a12438d0cdfa9a7de65dd012cd15d
Edward Cardoza Mesina <ed.cardoza@duocuc.cl>
2026-05-27 10:21:52 -0400

    Delete gateway directory

--- gateway/src/main/java/com/tiendaropa/gateway/exception/RouteNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.tiendaropa.gateway.exception;
-
-public class RouteNotFoundException extends RuntimeException {
-
-    public RouteNotFoundException(String path) {
-        super("Ruta no encontrada en el gateway: " + path);
-    }
-}
